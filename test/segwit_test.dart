b5bcfb5f3b93dbcb86c0b215053db834bb624104
Aaron Feickert <66188213+AaronFeickert@users.noreply.github.com>
2023-06-09 15:01:43 -0500

    ci: add CI (#3)

--- test/segwit_test.dart
+++ test/segwit_test.dart
@@ -1,7 +1,6 @@
+import 'package:bech32/bech32.dart';
 import 'package:hex/hex.dart';
 import 'package:test/test.dart';
-
-import 'package:bech32/bech32.dart';
 
 void main() {
   group('segwit with', () {
